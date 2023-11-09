d97d1936119c51c1632da32c059a97828bc87629
fParrax <franklinlpl@gmail.com>
2023-11-09 18:30:36 -0400

    g

--- src/Frames/ProcesarResultados.java
+++ src/Frames/ProcesarResultados.java
@@ -143,7 +143,6 @@ public class ProcesarResultados extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-               
                 new ProcesarResultados().setVisible(true);
             }
         });
